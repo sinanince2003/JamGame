38c39f47b49a2ab5820d4dbaed834ebc2a0cce9e
davutcan15081 <99992288+davutcan15081@users.noreply.github.com>
2022-02-20 18:56:30 +0300

    Add files via upload
    GÜNCEL 20/02/2022

--- Ucma.cs
+++ Ucma.cs
@@ -6,24 +6,35 @@ public class Ucma : MonoBehaviour
 {
     // Start is called before the first frame update
     public Animator anim;
+    public GameObject oyuncu;
+    Vector3 yenikoord,eskikoord;
     void Start()
     {
-       
+        
     }
+
     IEnumerator ucsaniyebekle()
     {
+       
         anim.SetBool("ucma", true);
+        yenikoord = new Vector3(transform.position.x, 10f, transform.position.z);
+        eskikoord = new Vector3(0,0,0);
+
         yield return new WaitForSecondsRealtime(3f);
         anim.SetBool("ucma", false);
-
+        transform.position = Vector3.MoveTowards(transform.position, eskikoord, .5f);
     }
 
 
     private void FixedUpdate()
     {
+
         if (Input.GetKey(KeyCode.W))
         {
             StartCoroutine(ucsaniyebekle());
+            transform.position = Vector3.MoveTowards(transform.position, yenikoord, .5f);
+
         }
+
     }
 }
